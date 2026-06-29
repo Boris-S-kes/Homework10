efb468154322d74824fdc67b9f9da48344d016d2
msi <https://github.com/Boris-S-kes/Homework10.git>
2026-06-29 15:22:08 +0400

    ДЗ 10

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         System.out.println("Hello world!");
+        String firstName = " Ivan ";
+        String middleName = " Ivanovich ";
+        String lastName = " Ivanov ";
+        String fullName = " Ivanov Ivan Ivanovich ";
+        System.out.println( "Ф. И. О. сотрудника — " + fullName);
+
+        fullName = "ivanov ivan ivanovich ";
+        System.out.println( "Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        fullName = "Иванов Семён Семёнович ";
+       String s = fullName.replace('ё' ,'е');
+       System.out.println( " Данные Ф. И. О. сотрудника — " +s);
+
 
         }
     }
